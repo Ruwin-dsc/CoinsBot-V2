1d22c7838eb59c3196edcde8bf1310fbbee55928
! Ruwin.zkr <110337782+Ruwin-dsc@users.noreply.github.com>
2023-07-19 10:56:48 +0200

    Update unblock.js

--- commands/administration/unblock.js
+++ commands/administration/unblock.js
@@ -13,13 +13,13 @@ exports.run = async (bot, message, args, color) => {
     if(!args[0]) return message.reply(`Veuillez précisez si vous souhaitez débloquer les ajouts d'argent \`add\` ou les \`remove\` d'argent ou les \`reset\`!`)
 
     if(args[0] == "add") {
-        bot.db.query(`UPDATE commands SET adds = "off" WHERE guildId = '${message.guild.id}'`)
+        bot.db.query(`UPDATE commands SET adds = "on" WHERE guildId = '${message.guild.id}'`)
         message.reply(`La commande \`${bot.prefix}add\` a été débloqué !`)
     } else if(args[0] == "remove") {
-        bot.db.query(`UPDATE commands SET remove = "off" WHERE guildId = '${message.guild.id}'`)
+        bot.db.query(`UPDATE commands SET remove = "on" WHERE guildId = '${message.guild.id}'`)
         message.reply(`La commande \`${bot.prefix}remove\` a été débloqué !`)
     } else if(args[0] == "reset") {
-        bot.db.query(`UPDATE commands SET reset = "off" WHERE guildId = '${message.guild.id}'`)
+        bot.db.query(`UPDATE commands SET reset = "on" WHERE guildId = '${message.guild.id}'`)
         message.reply(`La commande \`${bot.prefix}reset\` a été débloqué !`)
     }
 
