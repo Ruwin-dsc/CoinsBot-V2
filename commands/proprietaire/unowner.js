dec673cecfd71de18bb774c09640dea08d4cbd8b
! Ruwin.zkr <110337782+Ruwin-dsc@users.noreply.github.com>
2023-08-09 11:23:33 +0200

    Update unowner.js

--- commands/proprietaire/unowner.js
+++ commands/proprietaire/unowner.js
@@ -12,11 +12,11 @@ exports.help = {
 exports.run = async (bot, message, args, color) => {
     const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
         if (!user) return message.channel.send('`❌` `ERROR:` Pas de membre trouvé !')
-        bot.db.query(`SELECT * FROM list2 WHERE guildId = "${message.guild.id}" AND whitelistId = "${user.id}"`, async (err, req) => {
+        bot.db.query(`SELECT * FROM list2 WHERE guildId = "${message.guild.id}" AND ownerId = "${user.id}"`, async (err, req) => {
             if (req.length < 1) {
                 return message.channel.send("`❌` `ERROR:` Cette utilisateur n'est pas owner du bot !")
             } else {
-                bot.db.query(`DELETE FROM list2 WHERE guildId = ${message.guild.id} AND whitelistId = ${user.id}`);
+                bot.db.query(`DELETE FROM list2 WHERE guildId = ${message.guild.id} AND ownerId = ${user.id}`);
                 
                 message.reply(`\`✅\` ${user.user.username} n'est plus owner du bot !`)
             }
