d4d7e9ca0c81fc6e2621c18d5d29bcabe9b4ae7d
! Ruwin.zkr <110337782+Ruwin-dsc@users.noreply.github.com>
2023-08-25 14:36:44 +0200

    FIx

--- commands/gestioncoins/top.js
+++ commands/gestioncoins/top.js
@@ -9,21 +9,19 @@ exports.help = {
 }
 
 exports.run = async (bot, message, args, color) => {
-     bot.db.query(`SELECT * FROM user WHERE guildId = '${message.guild.id}' ORDER BY coins DESC LIMIT 10`, async (err, req) => {
+     bot.db.query(`SELECT * FROM user WHERE guildId = '${message.guild.id}'`, async (err, req) => {
      let leaderboardDescription = ""
      let countcoins1 = 1
-     let newusername
-      req.forEach((row, index) => {
-        const username = row.userId;
-        const coins = row.coins;
-        newusername = message.guild.members.cache.get(username)
-        if(newusername == undefined) newusername = "Utilisateur introuvable"
-        if(coins !== "0") {
-        leaderboardDescription += `${countcoins1}) ${newusername}\n\`${coins} coins\` :coin:\n`;
-        countcoins1 = Number(countcoins1) + 1
-        }
-      });
-
+     const Ordre2 = req.sort((a, b) => b.coins - a.coins);
+     for (const row of Ordre2.slice(0, 10)) {
+      const userId = row.userId;
+      const coins = row.coins;
+      const user = await message.guild.members.cache.get(userId);
+      if (user && coins !== "0") {
+        leaderboardDescription += `${countcoins1}) ${user.user.username}\n\`${coins} coins\` :coin:\n`;
+        countcoins1++;
+      } 
+    }
       const lbCoins = new Discord.EmbedBuilder()
         .setAuthor({ name: `Leaderboard des coins sur ${message.guild.name}`, iconURL: `https://images-ext-2.discordapp.net/external/Db_tN_Y54YNEZmqAFsDmqIqQT0PwwNIiJCWGac69E-o/https/images.emojiterra.com/twitter/v13.0/512px/1fa99.png`, url: 'https://discord.gg/zcN3sB5KSv' })
         .setDescription(leaderboardDescription || "Aucune données")
@@ -72,20 +70,19 @@ exports.run = async (bot, message, args, color) => {
 
             if (i.values[0] == "top_bank") {
               if(i.user.id !== message.author.id) return i.reply({ content: `Vous n'avez pas la permission !`, ephemeral: true})
-                 bot.db.query(`SELECT * FROM user ORDER BY banque DESC LIMIT 10`, async (err, req) => {
+                 bot.db.query(`SELECT * FROM user`, async (err, req) => {
+                  const Ordre = req.sort((a, b) => b.banque - a.banque);
      let leaderboardDescriptionBank = ""
      let countcoins = 1
-     let newusername;
-      req.forEach((row, index) => {
-        const username = row.userId;
-        const coins = row.banque;
-        newusername = message.guild.members.cache.get(username)
-        if(newusername == undefined) newusername = "Utilisateur introuvable"
-        if(coins !== "0") {
-        leaderboardDescriptionBank += `${countcoins}) ${newusername}\n\`${coins} coins\` :bank:\n`;
-        countcoins = Number(countcoins) + 1
-        }
-      });
+     for (const row of Ordre.slice(0, 10)) {
+      const userId = row.userId;
+      const coins = row.coins;
+      const user = await message.guild.members.cache.get(userId);
+      if (user && coins !== "0") {
+        leaderboardDescriptionBank += `${countcoins}) ${user.user.username}\n\`${coins} coins\` :coin:\n`;
+        countcoins++;
+      } 
+    }
 
       const lbBank = new Discord.EmbedBuilder()
         .setAuthor({ name: `Leaderboard des coins en banque sur ${message.guild.name}`, iconURL: `https://images-ext-2.discordapp.net/external/dimTtGJ41YBkBAWgxAAvRax2OaAl27krgLKswvAFCF8/https/www.emoji.co.uk/files/mozilla-emojis/travel-places-mozilla/11821-bank.png`, url: 'https://discord.gg/zcN3sB5KSv' })
@@ -100,21 +97,19 @@ exports.run = async (bot, message, args, color) => {
 
             if (i.values[0] == "top_coins") {
               if(i.user.id !== message.author.id) return i.reply({ content: `Vous n'avez pas la permission !`, ephemeral: true})
-                 bot.db.query(`SELECT * FROM user ORDER BY coins DESC LIMIT 10`, async (err, req) => {
+                 bot.db.query(`SELECT * FROM user`, async (err, req) => {
+                  const Ordre = req.sort((a, b) => b.coins - a.coins);
      let leaderboardDescriptionCoins = ""
      let coinsCount = 1
-     let newusername
-      req.forEach((row, index) => {
-        username = row.userId;
-        const coins = row.coins;
-        newusername = message.guild.members.cache.get(username)
-        if(newusername == undefined) newusername = "Utilisateur introuvable"
-        if(username == undefined) username = "Utilisateur introuvable"
-        if(coins !== "0") {
-        leaderboardDescriptionCoins += `${coinsCount}) ${newusername}\n\`${coins} coins\` :coin:\n`;
-        coinsCount = Number(coinsCount) + 1
-        }
-      });
+     for (const row of Ordre.slice(0, 10)) {
+      const userId = row.userId;
+      const coins = row.coins;
+      const user = await message.guild.members.cache.get(userId);
+      if (user && coins !== "0") {
+        leaderboardDescriptionCoins += `${coinsCount}) ${user.username}\n\`${coins} coins\` :coin:\n`;
+        coinsCount++;
+      } 
+    }
 
       const lbCoins = new Discord.EmbedBuilder()
         .setAuthor({ name: `Leaderboard des coins sur ${message.guild.name}`, iconURL: `https://images-ext-2.discordapp.net/external/Db_tN_Y54YNEZmqAFsDmqIqQT0PwwNIiJCWGac69E-o/https/images.emojiterra.com/twitter/v13.0/512px/1fa99.png`, url: 'https://discord.gg/zcN3sB5KSv' })
@@ -129,20 +124,19 @@ exports.run = async (bot, message, args, color) => {
 
             if (i.values[0] == "top_rep") {
               if(i.user.id !== message.author.id) return i.reply({ content: `Vous n'avez pas la permission !`, ephemeral: true})
-                 bot.db.query(`SELECT * FROM user ORDER BY reputation DESC LIMIT 10`, async (err, req) => {
-     let leaderboardDescriptionReputation = ""
-     let repCount = 1
-     let newusername
-      req.forEach((row, index) => {
-        const username = row.userId;
-        const coins = row.reputation;
-        newusername = message.guild.members.cache.get(username)
-        if(newusername == undefined) newusername = "Utilisateur introuvable"
-        if(coins !== "0") {
-        leaderboardDescriptionReputation += `${repCount}) ${newusername}\n\`${coins} rep\` :small_red_triangle:\n`;
-        repCount = Number(repCount) + 1
-        }
-      });
+                 bot.db.query(`SELECT * FROM user`, async (err, req) => {
+                  const Ordre = req.sort((a, b) => b.reputation - a.reputation);
+          let leaderboardDescriptionReputation = ""
+          let repCount = 1
+          for (const row of Ordre.slice(0, 10)) {
+            const userId = row.userId;
+            const coins = row.reputation;
+            const user = await message.guild.members.cache.get(userId);
+            if (user && coins !== "0") {
+              leaderboardDescriptionReputation += `${repCount}) ${user.user.username}\n\`${coins} coins\` :coin:\n`;
+              repCount++;
+            } 
+          }
 
       const lbRep = new Discord.EmbedBuilder()
         .setAuthor({ name: `Leaderboard des réputations sur ${message.guild.name}`, iconURL: `https://images-ext-2.discordapp.net/external/dimTtGJ41YBkBAWgxAAvRax2OaAl27krgLKswvAFCF8/https/www.emoji.co.uk/files/mozilla-emojis/travel-places-mozilla/11821-bank.png`, url: 'https://discord.gg/zcN3sB5KSv' })
